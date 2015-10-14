4d0b845590180f25456fd151d862a8e190902b17
LensHunnel <lens-hunnel.mbina-mbina@cpe.fr>
2015-10-14 12:15:14 +0200

    Rajout du dictionnaire

--- src/main/Dictionnary.java
+++ src/main/Dictionnary.java
@@ -15,13 +15,18 @@ public class Dictionnary {
 	public boolean wordExiste(String word){
 		boolean b=false;
 		try{
-			InputStream ips=new FileInputStream(path); 
+			String dico_first_word = "src\\main\\"+path+'\\'+word.charAt(0)+".txt";
+			System.out.println("chemin dico :" + dico_first_word);
+			InputStream ips=new FileInputStream(dico_first_word); 
 			InputStreamReader ipsr=new InputStreamReader(ips);
 			BufferedReader br=new BufferedReader(ipsr);
 			String ligne;
 			while ((ligne=br.readLine())!=null){
-				if(ligne==word)
+				//
+				
+				if(ligne.compareToIgnoreCase(word)==0)
 				{
+					System.out.println(ligne);
 					b= true;
 				}
 				
